36a1d4d96db4d9226387ac1e20632667d73b0d2b
typej2003 <typej2003@gmail.com>
2025-12-05 07:35:28 -0400

    actualizar

--- resources/views/residencial.blade.php
+++ resources/views/residencial.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Navbar de Tres Secciones - Vista SOLO PC</title>
+    <title>RedNetVe</title>
     
     <!-- Enlace a Bootstrap 5 CSS CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" xintegrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjHh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
