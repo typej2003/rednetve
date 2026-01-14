c0c19ab7cff83e8c6ac9958912a8a730de56299d
typej2003 <typej2003@gmail.com>
2026-01-13 23:15:28 -0400

    subir

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -9,6 +9,7 @@
     <title>@yield('title', 'RedNetVe - Bienvenidos')</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
