8d96ca83c4aefe840ced8c491f805893e8b0e54d
typej2003 <typej2003@gmail.com>
2026-01-14 15:44:04 -0400

    subir

--- app/Http/Livewire/Admin/SolicitarCita.php
+++ app/Http/Livewire/Admin/SolicitarCita.php
@@ -61,7 +61,7 @@ class SolicitarCita extends Component
 
         $email = new EmailController();
 
-        $emailwelcome->sendMailAgendaCliente($cita);
+        $email->sendMailAgendaCliente($cita);
 
         // $user = User::where('role', 'root')->first();
         $user = User::where('role', 'vendedor')->first();        
